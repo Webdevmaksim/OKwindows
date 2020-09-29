c266e504f86420f68d95507160d36d9ef62fb5d0
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2020-09-29 16:47:42 +0300

    minor changes

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ $(document).ready(function () {
         }
       });
     // Set the date we're counting down to
-    var countDownDate = new Date("sep 29, 2020 15:37:25").getTime();
+    var countDownDate = new Date("oct 30, 2020 15:37:25").getTime();
     var cabinDownDate = new Date("nov 29, 2020 18:38:20").getTime();
     var windowDownDate = new Date("oct 8, 2020 10:44:13").getTime();
     
